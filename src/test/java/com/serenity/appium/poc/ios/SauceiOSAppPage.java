4ac72033f0a5f8cbfbec12d106b678b60cd4bd01
vikramvi <vikram@noa.one>
2017-01-24 12:33:19 +0100

    fix as per https://github.com/serenity-bdd/serenity-core/issues/660

--- src/test/java/com/serenity/appium/poc/ios/SauceiOSAppPage.java
+++ src/test/java/com/serenity/appium/poc/ios/SauceiOSAppPage.java
@@ -1,11 +1,14 @@
 package com.serenity.appium.poc.ios;
 
 import net.thucydides.core.pages.PageObject;
+import net.thucydides.core.webdriver.WebDriverFacade;
 
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.pagefactory.iOSFindBy;
 
 public class SauceiOSAppPage extends PageObject{
@@ -16,14 +19,21 @@ public class SauceiOSAppPage extends PageObject{
     //@iOSFindBy(xpath="//XCUIElementTypeApplication[1]//XCUIElementTypeTextField[contains(@name,'fbemail')]")
     //private MobileElement emailTextField; 
     
+    //https://github.com/serenity-bdd/serenity-core/issues/660
+    //http://appium.github.io/java-client/io/appium/java_client/ios/IOSDriver.html#method.summary
+    public IOSDriver<IOSElement> iOSDriver() {
+	return (IOSDriver<IOSElement>) ((WebDriverFacade) getDriver()).getProxiedDriver();
+   }
+    
     public void inputEmail(){
 	if(element(emailTextField).isDisplayed()){
-	element(emailTextField).type("iOS@apple.net");
-	//element(emailTextField).sendKeys(Keys.RETURN);	 not working on emulator
-	//getDriver().hideKeyboard(); ??
-	System.out.println("test");
+        	element(emailTextField).type("iOS@apple.net");
+        	//element(emailTextField).sendKeys(Keys.RETURN);	 not working on emulator
+        	//getDriver().hideKeyboard(); ??
+        	iOSDriver().hideKeyboard();
+        	System.out.println("test");
 	}else{
-	    System.out.println("test");
+	        System.out.println("test");
 	}
     }
     
