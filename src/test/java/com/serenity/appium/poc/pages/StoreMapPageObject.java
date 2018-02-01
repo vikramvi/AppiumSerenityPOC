6087ad4c69481e3dfa960128385f31fb8e6c34d6
Joe Hennessey <4jp.hennessey@gmail.com>
2018-02-01 14:40:08 -0500

    1. trying to resync store map page object after doing "git add"

---
+++ src/test/java/com/serenity/appium/poc/pages/StoreMapPageObject.java
@@ -0,0 +1,44 @@
+package com.serenity.appium.poc.pages;
+
+import com.serenity.appium.poc.pages.MobilePageObject;
+import com.serenity.appium.poc.utils.Utils;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSFindBy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class StoreMapPageObject extends MobilePageObject {
+
+    @AndroidFindBy(accessibility = "touchableImage-store-map")
+    @iOSFindBy(accessibility = "touchableImage-store-map")
+    private WebElement TOUCHABLE_IMAGE_storeMap;
+
+    @AndroidFindBy(accessibility = "Tap to speak")
+    @iOSFindBy(accessibility = "Tracking")
+    private WebElement ICON_mapIdentifier;
+
+    public boolean isStoreMapThumbnailPresent() {
+        try {
+            return TOUCHABLE_IMAGE_storeMap.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public boolean clickStoreMapThumbnail() {
+        boolean successLevel = Utils.tryClicking(TOUCHABLE_IMAGE_storeMap);
+        Utils.tryClickingAllow();
+        return successLevel;
+    }
+
+    public boolean isActualStoreMapLoaded() {
+        try {
+            return ICON_mapIdentifier.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public StoreMapPageObject(WebDriver driver) {
+        super(driver);
+    }
+}
