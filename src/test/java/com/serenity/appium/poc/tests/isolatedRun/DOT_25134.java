b9baae1d94dd9bc832c412a3d1e0e2c2be94efc0
Joe Hennessey <4jp.hennessey@gmail.com>
2018-08-02 16:26:04 -0400

    1. adding new test for isolated run

---
+++ src/test/java/com/serenity/appium/poc/tests/isolatedRun/DOT_25134.java
@@ -0,0 +1,27 @@
+package com.serenity.appium.poc.tests.isolatedRun;
+
+import com.serenity.appium.poc.WineAppTest;
+import com.serenity.appium.poc.utils.FindStore;
+import com.serenity.appium.poc.utils.Properties;
+import com.serenity.appium.poc.utils.Regression1;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class DOT_25134 extends WineAppTest {
+
+    @Test // verified on iOS, Android on 5/22/18
+    @Category({Regression1.class, FindStore.class})
+    public void DOT_25134_verifyClosestStoreOnHomepage(){
+        Properties properties = new Properties();
+        String expectedTitle = "BOYNTON BEACH";
+        if (properties.isSauceLabsRun()) {
+            expectedTitle = "FREMONT";
+        }
+        try{
+            appSteps.completeOnboardingAllowingLocation();
+            appSteps.verifyHomepageStoreDetails(expectedTitle);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
